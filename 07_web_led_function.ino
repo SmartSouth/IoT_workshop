46d984d5d860e62f8de88bfd1aea0f7e53a40faf
Smart South <greg@smartsouth.uk>
2016-01-27 00:45:26 +0000

    Update 07_web_led_function.ino

--- 07_web_led_function.ino
+++ 07_web_led_function.ino
@@ -1,11 +1,28 @@
-int button_pin=D1;
+int led1 = D0; //create variable for the LED pin
+int state = 0;
 
-void setup(){
-pinMode(button_pin, INPUT_PULLDOWN);
+void setup()
+{
+   pinMode(led1, OUTPUT);
+   Spark.function("led",ledToggle);
+   digitalWrite(led1, LOW);
 }
-void loop(){
-int button_state = digitalRead(button_pin);
-if(button_state == HIGH){
-    Spark.publish("switch");
+
+void loop()
+{
+   // Nothing to do here
+}
+
+int ledToggle(String command) {
+	if (command=="toggle") {
+   		if (state == 0){
+			state = 1;
+			digitalWrite(led1, HIGH);
+			return 1;
+		} else {
+			state = 0;
+			digitalWrite(led1, LOW);
+			return 0;
+			}
     }
 }
