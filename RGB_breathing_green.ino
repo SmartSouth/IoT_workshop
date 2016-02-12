3ec1cf1eaf78aeaec321b018eea41a5f79ec6197
Smart South <greg@smartsouth.uk>
2016-02-12 08:53:13 +0000

    Create RGB_breathing_green.ino

---
+++ RGB_breathing_green.ino
@@ -0,0 +1,20 @@
+void setup() {
+    Serial.begin(9600);
+    RGB.control(true);
+    RGB.color(0,255,0);
+    RGB.brightness(0);
+}
+
+void loop() {
+     int x = 1;
+    for(int i=0; i<=255; i=i+x){
+        RGB.brightness(i);
+        if(i==254){
+         x=-1;       
+         } else if(i==0){
+         x=1;     
+         }
+        delay(10);
+    }
+
+}
