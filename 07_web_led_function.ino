904c3cc519e22aa611d6b5461efcbc6f957afb1d
Smart South <greg@smartsouth.uk>
2016-02-13 05:22:11 +0000

    Update 07_web_led_function.ino

--- 07_web_led_function.ino
+++ 07_web_led_function.ino
@@ -1,11 +1,10 @@
-int led1 = D7;
-int state = 0;
+int led = D7;
 
 void setup()
 {
-   pinMode(led1, OUTPUT);
+   pinMode(led, OUTPUT);
    Spark.function("led",ledToggle);
-   digitalWrite(led1, LOW);
+   digitalWrite(led, LOW);
 
 }
 
@@ -17,13 +16,8 @@ void loop()
 int ledToggle(String command) {
 
 	if (command=="on") {
-			digitalWrite(led1, HIGH);
-		    return 1;
+			digitalWrite(led, HIGH);
 		} else if (command=="off") {
-			digitalWrite(led1, LOW);
-			state = 0; 
-			return 0;
-			} else {
-			    return -1;
+			digitalWrite(led, LOW);
 			}
 		}
