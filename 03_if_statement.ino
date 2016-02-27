c18118b6a6d131d9918754b79cbf1d0edb816251
Smart South <greg@smartsouth.uk>
2016-02-27 13:07:49 +0000

    Update 03_if_statement.ino

--- 03_if_statement.ino
+++ 03_if_statement.ino
@@ -1,14 +1,14 @@
-int pin=D0;
+int button=D0;
 int led=D1;
 int button_state;
 
 void setup(){
-pinMode(pin, INPUT_PULLDOWN);		
+pinMode(button, INPUT_PULLDOWN);		
 pinMode(led, OUTPUT);
 }
 
 void loop(){
-button_state=digitalRead(pin);
+button_state=digitalRead(button);
 if(button_state==HIGH){
 	digitalWrite(led, HIGH)
 } else {
